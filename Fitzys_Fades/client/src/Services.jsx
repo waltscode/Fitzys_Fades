2a48075eaf0815faac53e7f0d757157e546f4de3
Gym Freak <guminyj@gmail.com>
2024-03-14 21:37:04 -0400

    got all the styling done for the service page now im going to work on about page also might add some stuff to service page later

--- Fitzys_Fades/client/src/Services.jsx
+++ Fitzys_Fades/client/src/Services.jsx
@@ -6,38 +6,38 @@ function Services() {
         <h2 className="text-center text-4xl font-semibold">Our Services</h2>
         <div className="flex flex-wrap justify-center">
           
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-4 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="https://via.placeholder.com/200" alt="Placeholder" className="rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">Traditional<br></br>Mens Cut</h2>
-                <p className="text-gray-600 mb-2 text-right">$45</p>
+              <img src="images/traditional.jpg" alt="Placeholder" className="rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Traditional<br></br>Mens Cut</h2>
+                <p className="text-teal-200 mb-2 text-right">$45</p>
               </div>
             </div>
           </div>
         </div>
    
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-8 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="https://via.placeholder.com/200" alt="Placeholder" className="rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">Straight Razor</h2>
-                <p className="text-gray-600 mb-2 text-right">$45</p>
+              <img src="images/straight_razor1.jpg" alt="Placeholder" className="rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Straight Razor</h2>
+                <p className="text-teal-200 mb-2 text-right">$45</p>
               </div>
             </div>
           </div>
         </div>
 
            
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-4 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="images/Kidz_Kutz.jpg" alt="Placeholder" className=" object-contain rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">Kidz Kutz</h2>
-                <p className="text-gray-600 mb-2 text-right">$30</p>
+              <img src="images/Kidz_Kutz2.jpg" alt="Picture of a child with a fade and design in the left hand side of his head" className=" object-contain rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Kidz Kutz</h2>
+                <p className="text-teal-200 mb-2 text-right">$30</p>
               </div>
             </div>
           </div>
@@ -45,39 +45,39 @@ function Services() {
 
 
           
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-4 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="https://via.placeholder.com/200" alt="Placeholder" className="rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">Braid-UP</h2>
-                <p className="text-gray-600 mb-2 text-right">$50</p>
+              <img src="images/braid_up1.jpg" alt="this is a X style braid up" className="rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Braid-UP</h2>
+                <p className="text-teal-200 mb-2 text-right">$50</p>
               </div>
             </div>
           </div>
         </div>
 
            
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-4 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="images/straightRazor.jpg" alt="Placeholder" className="rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">fitzy's fade</h2>
-                <p className="text-gray-600 mb-2 text-right">$60</p>
+              <img src="images/fitzy_fades1.jpg" alt="someone getting a straight razor shave" className="rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Fitzys Fade</h2>
+                <p className="text-teal-200 mb-2 text-right">$60</p>
               </div>
             </div>
           </div>
         </div>
 
            
-        <div className="flex flex-col bg-white rounded-lg shadow-md p-4 mr-4 mb-4">
+        <div className="flex flex-col bg-cyan-600 rounded-lg shadow-md p-2 mr-4 mb-14">
           <div className="relative">
             <div className="relative">
-              <img src="https://via.placeholder.com/200" alt="Placeholder" className="rounded-md z-1"></img>
-              <div className="absolute bottom-0 right-0 p-2 z-2">
-                <h2 className="text-xl font-semibold mb-2">Custom Designs</h2>
-                <p className="text-gray-600 mb-2 text-right">$65</p>
+              <img src="images/custom_design1.jpg" alt="Placeholder" className="rounded-md z-1"></img>
+              <div className="absolute top-0 right-0 p-2 z-2">
+                <h2 className="text-xl text-amber-400 font-semibold mb-2">Custom Designs</h2>
+                <p className="text-teal-200 mb-2 text-right">$65</p>
               </div>
             </div>
           </div>
