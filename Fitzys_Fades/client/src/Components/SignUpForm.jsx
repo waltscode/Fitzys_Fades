843edf6a5739d1085b24e6b8f89bcc0a31d00089
JohnM89 <mjammer2@gmail.com>
2024-03-16 03:00:56 -0400

    commited signup form with missing input feilds! (fixed now hopefully)

--- Fitzys_Fades/client/src/Components/SignUpForm.jsx
+++ Fitzys_Fades/client/src/Components/SignUpForm.jsx
@@ -22,8 +22,8 @@ const SignupForm = ({ onClose }) => {
       }
       const { token } = await response.json();
       Auth.login(token);
-      if (onClose) onClose(); // should close the modal 
-      navigate('/'); // redirection, might need to be modified
+      if (onClose) onClose(); // closes the modal on successful signup
+      navigate('/'); // redirects, but might need to be set to something else may break modal..
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -34,12 +34,45 @@ const SignupForm = ({ onClose }) => {
     <div className="relative bg-white p-6 rounded-lg">
       {showAlert && (
         <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
-          Sign-up Error!
+          Sign-up Err0r!
         </div>
       )}
       <button onClick={onClose} className="absolute top-0 right-0 p-4">X</button>
       <form onSubmit={handleFormSubmit} className="space-y-6">
-        
+        <input
+          className="w-full p-2 border border-gray-300 rounded-md"
+          type="text"
+          placeholder="Username"
+          name="username"
+          onChange={handleInputChange}
+          value={userFormData.username}
+          required
+        />
+        <input
+          className="w-full p-2 border border-gray-300 rounded-md"
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleInputChange}
+          value={userFormData.email}
+          required
+        />
+        <input
+          className="w-full p-2 border border-gray-300 rounded-md"
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleInputChange}
+          value={userFormData.password}
+          required
+        />
+        <button
+          className="w-full p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          type="submit"
+          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+        >
+          Sign Up
+        </button>
       </form>
     </div>
   );
