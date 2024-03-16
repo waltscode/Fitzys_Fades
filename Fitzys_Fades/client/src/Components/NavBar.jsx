c963632abc6f74dc84e439d92762f19cada587fa
JohnM89 <mjammer2@gmail.com>
2024-03-16 03:08:18 -0400

    reconfigured SignInForm, renamed functions and references for consistency with auth.js

--- Fitzys_Fades/client/src/Components/NavBar.jsx
+++ Fitzys_Fades/client/src/Components/NavBar.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
-import LoginForm from './LoginForm';
+import LogInForm from './LogInForm';
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
@@ -35,7 +35,7 @@ const AppNavbar = () => {
       {showSignInModal && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" onClick={() => setShowSignInModal(false)}>
           <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white" onClick={e => e.stopPropagation()}>
-            <SigninForm />
+            <LogInForm />
           </div>
         </div>
       )}
