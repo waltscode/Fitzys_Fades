9c9b794fa5eabb4556f85ea9083e1cf087fd591d
JohnM89 <mjammer2@gmail.com>
2024-03-16 03:09:11 -0400

    had useState commented out still

--- Fitzys_Fades/client/src/Components/NavBar.jsx
+++ Fitzys_Fades/client/src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 import LogInForm from './LogInForm';
