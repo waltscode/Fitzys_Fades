1a9f3ec5f9f43482539defbf598512c11e45fe55
JohnM89 <mjammer2@gmail.com>
2024-03-16 02:32:05 -0400

    updated the NavBar for Tailwind compatability

--- Fitzys_Fades/client/src/Components/NavBar.jsx
+++ Fitzys_Fades/client/src/Components/NavBar.jsx
@@ -1,34 +1,26 @@
 //import { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
   return (
-    <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            Fitzys Fades
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ms-auto'>
-              {/* link to Signup and Signin pages if not logged in */}
-              {!Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>
-                  <Nav.Link as={Link} to='/signin'>Sign In</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <div className="bg-gray-800 p-4">
+      <div className="flex items-center justify-between">
+        <Link to="/" className="text-white text-lg">
+          Fitzys Fades
+        </Link>
+        <div>
+          {!Auth.loggedIn() ? (
+            <>
+              <Link to="/signup" className="text-gray-300 hover:text-white mx-2">Sign Up</Link>
+              <Link to="/signin" className="text-gray-300 hover:text-white mx-2">Sign In</Link>
+            </>
+          ) : (
+            <button onClick={Auth.logout} className="text-gray-300 hover:text-white mx-2">Logout</button>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
