b1456be034f47fadd4c54012d8ec324d3e764235
JohnM89 <mjammer2@gmail.com>
2024-03-16 02:26:34 -0400

    refactored the Navbar to use direct links for auth, remove unused useState

--- Fitzys_Fades/client/src/Components/NavBar.jsx
+++ Fitzys_Fades/client/src/Components/NavBar.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
+//import { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignUpForm';
-import LoginForm from './SignInForm';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <>
       <Navbar bg='dark' variant='dark' expand='lg'>
@@ -19,47 +15,19 @@ const AppNavbar = () => {
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
             <Nav className='ms-auto'>
-              {Auth.loggedIn() ? (
+              {/* link to Signup and Signin pages if not logged in */}
+              {!Auth.loggedIn() ? (
                 <>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                  <Nav.Link as={Link} to='/signup'>Sign Up</Nav.Link>
+                  <Nav.Link as={Link} to='/signin'>Sign In</Nav.Link>
                 </>
               ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
     </>
   );
 };
