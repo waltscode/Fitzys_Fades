c765d97ade1199357a217f3f1f5305330e225fd1
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-03-22 19:55:18 -0400

    FrontEnd- Remove modal code. Added landing page.

--- Fitzys_Fades/client/src/Signin.jsx
+++ Fitzys_Fades/client/src/Signin.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
