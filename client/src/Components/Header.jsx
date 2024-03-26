15765fab1501189be17320eb771277bbeb76b733
waltscode <walterswork91@outlook.com>
2024-03-25 22:04:44 -0400

    edited utils auth file

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,7 +3,7 @@
 // import React from 'react';
 import '../styles/Header.css';
 import headerImage from '/images/textstripe.jpg';
-import { useAuth } from '../utils/LinkuthContext';
+import { useAuth } from '../utils/authContext';
 import {Link} from 'react-router-dom';
 
 function Header() {
